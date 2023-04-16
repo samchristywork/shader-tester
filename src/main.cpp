d6db04bb94dfa315498c096bc94185d172a1df21
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-16 16:07:53 -0500

    Feature: Adding multiple meshes to the same scene
    This program still only shows one mesh, but I am making strides towards
    showing multiple.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,24 @@
 #include <stb/stb_image.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <vector>
+
+struct MeshData {
+  GLuint VAO;
+  GLuint VBO;
+  GLuint EBO;
+  GLsizei indexCount;
+  std::vector<GLuint> indices;
+  std::string filename;
+  GLfloat x;
+  GLfloat y;
+  GLfloat z;
+};
+
+float playerx = 0;
+float playery = 0;
+float playerz = 5;
 
 void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
@@ -39,6 +56,14 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, GL_TRUE);
+  } else if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+    playerx += 1.1;
+  } else if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+    playerz += 1.1;
+  } else if (key == GLFW_KEY_S && action == GLFW_PRESS) {
+    playerx -= 1.1;
+  } else if (key == GLFW_KEY_D && action == GLFW_PRESS) {
+    playerz -= 1.1;
   }
 }
 
@@ -124,70 +149,80 @@ int main() {
   glDeleteShader(vertex_shader);
   glDeleteShader(fragment_shader);
 
-  Assimp::Importer importer;
-  const aiScene *scene =
-      importer.ReadFile("cube.obj", aiProcess_Triangulate | aiProcess_FlipUVs);
+  std::vector<MeshData> meshDataList(2);
+  meshDataList[0].filename = "blenderbox.obj";
+  meshDataList[1].filename = "blenderbox.obj";
+  meshDataList[0].x = 0.0f;
+  meshDataList[0].y = 0.0f;
+  meshDataList[0].z = 0.0f;
+  meshDataList[1].x = 1.0f;
+  meshDataList[1].y = 1.0f;
+  meshDataList[1].z = 1.0f;
 
-  std::vector<GLfloat> vertices;
-  std::vector<GLuint> indices;
+  for (unsigned int i = 0; i < 2; ++i) {
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile(
+        meshDataList[i].filename, aiProcess_Triangulate | aiProcess_FlipUVs);
 
-  if (scene && scene->HasMeshes()) {
-    for (unsigned int i = 0; i < scene->mNumMeshes; ++i) {
-      aiMesh *mesh = scene->mMeshes[i];
-      for (unsigned int j = 0; j < mesh->mNumVertices; ++j) {
+    std::vector<GLfloat> vertices;
 
-        // Positions
-        vertices.push_back(mesh->mVertices[j].x);
-        vertices.push_back(mesh->mVertices[j].y);
-        vertices.push_back(mesh->mVertices[j].z);
+    if (scene && scene->HasMeshes()) {
+      for (unsigned int i = 0; i < scene->mNumMeshes; ++i) {
+        aiMesh *mesh = scene->mMeshes[i];
+        for (unsigned int j = 0; j < mesh->mNumVertices; ++j) {
+          // Positions
+          vertices.push_back(mesh->mVertices[j].x + meshDataList[i].x);
+          vertices.push_back(mesh->mVertices[j].y + meshDataList[i].y);
+          vertices.push_back(mesh->mVertices[j].z + meshDataList[i].z);
 
-        // Texture coordinates
-        if (mesh->mTextureCoords[0]) {
-          vertices.push_back(mesh->mTextureCoords[0][j].x);
-          vertices.push_back(mesh->mTextureCoords[0][j].y);
-        } else {
-          vertices.push_back(0.0f);
-          vertices.push_back(0.0f);
+          // Texture coordinates
+          if (mesh->mTextureCoords[0]) {
+            vertices.push_back(mesh->mTextureCoords[0][j].x);
+            vertices.push_back(mesh->mTextureCoords[0][j].y);
+          } else {
+            vertices.push_back(0.0f);
+            vertices.push_back(0.0f);
+          }
+        }
+
+        for (unsigned int k = 0; k < mesh->mNumFaces; ++k) {
+          aiFace face = mesh->mFaces[k];
+          for (unsigned int l = 0; l < face.mNumIndices; ++l) {
+            meshDataList[i].indices.push_back(face.mIndices[l]);
+          }
         }
       }
-
-      for (unsigned int k = 0; k < mesh->mNumFaces; ++k) {
-        aiFace face = mesh->mFaces[k];
-        for (unsigned int l = 0; l < face.mNumIndices; ++l) {
-          indices.push_back(face.mIndices[l]);
-        }
-      }
+    } else {
+      printf("Scene not loaded.\n");
+      exit(EXIT_FAILURE);
     }
-  } else {
-    printf("Scene not loaded.\n");
-    exit(EXIT_FAILURE);
+
+    glGenVertexArrays(1, &meshDataList[i].VAO);
+    glGenBuffers(1, &meshDataList[i].VBO);
+    glGenBuffers(1, &meshDataList[i].EBO);
+
+    glBindVertexArray(meshDataList[i].VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, meshDataList[i].VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat),
+                 &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshDataList[i].EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                 meshDataList[i].indices.size() * sizeof(GLuint),
+                 &meshDataList[i].indices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
+                          (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
+                          (GLvoid *)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    vertices.clear();
   }
-
-
-  GLuint VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0],
-               GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),
-               &indices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
-                        (GLvoid *)0);
-  glEnableVertexAttribArray(0);
-
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
-                        (GLvoid *)(3 * sizeof(GLfloat)));
-  glEnableVertexAttribArray(1);
-
-  glBindVertexArray(0);
 
   GLuint texture;
   int width, height, nrChannels;
@@ -238,10 +273,6 @@ int main() {
 
   glGenerateMipmap(GL_TEXTURE_2D);
 
-  float x = 0.0f;
-  float y = 0.0f;
-  float z = 0.0f;
-
   float angle = 0.0f;
 
   int screenWidth = 800;
@@ -267,41 +298,46 @@ int main() {
 
     glUniform1i(ourTextureLocation, 0);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(x, y, z));
-    model =
-        glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.0f, 0.0f));
+    for (int i = 0; i < 2; i++) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model =
+          glm::translate(model, glm::vec3(meshDataList[i].x, meshDataList[i].y,
+                                          meshDataList[i].z));
+      model =
+          glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.0f, 0.0f));
 
-    // View matrix
-    glm::vec3 cameraPos = glm::vec3(playerx, playery, playerz);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
+      // View matrix
+      glm::vec3 cameraPos = glm::vec3(playerx, playery, playerz);
+      glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+      glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+      glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
 
-    // Projection matrix
-    float aspectRatio = (float)screenWidth / (float)screenHeight;
-    float fov = glm::radians(45.0f);
-    float nearPlane = 0.1f;
-    float farPlane = 100.0f;
-    glm::mat4 projection =
-        glm::perspective(fov, aspectRatio, nearPlane, farPlane);
+      // Projection matrix
+      float aspectRatio = (float)screenWidth / (float)screenHeight;
+      float fov = glm::radians(45.0f);
+      float nearPlane = 0.1f;
+      float farPlane = 100.0f;
+      glm::mat4 projection =
+          glm::perspective(fov, aspectRatio, nearPlane, farPlane);
 
-    // Pass the matrices to the shader
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+      // Pass the matrices to the shader
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+      glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+      glBindVertexArray(meshDataList[i].VAO);
+      glDrawElements(GL_TRIANGLES, meshDataList[i].indices.size(),
+                     GL_UNSIGNED_INT, 0);
+    }
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteProgram(shader_program);
+  // glDeleteVertexArrays(1, &VAO);
+  // glDeleteBuffers(1, &VBO);
+  // glDeleteProgram(shader_program);
 
   glfwTerminate();
 }
